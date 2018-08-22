d8858cba457d17edce60641cff95d2d604bc1005
Irving Didier <bider88@gmail.com>
2018-08-22 15:51:08 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// AGM
+import { AgmCoreModule } from '@agm/core';
+
 import { AppComponent } from './app.component';
+import { config } from './config';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: config.apiKey
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
